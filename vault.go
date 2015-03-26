1d44f7d417b5af794c3b58a0b8d79ff0caf71df9
CoderBlaine <blaine@worldweb.com>
2015-03-26 08:43:31 -0600

    Added: Vault Struct Comments
    Added comments to vault.go to pass golint

--- vault.go
+++ vault.go
@@ -8,12 +8,14 @@ import (
 // https://developer.paypal.com/docs/api/#vault
 
 type (
+	// VaultRequest maps to vault_request object
 	VaultRequest struct {
 		CreditCard
 		MerchantID     string `json:"merchant_id,omitempty"`
 		ExternalCardID string `json:"external_card_id,omitempty"`
 	}
 
+	// VaultResponse maps to vault_response object
 	VaultResponse struct {
 		VaultRequest
 		CreateTime *time.Time `json: "create_time"`
